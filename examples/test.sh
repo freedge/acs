1abad4e13425468e2be23d834bcc54a434fc8d35
François Rigault <rigault.francois@gmail.com>
2020-08-16 11:11:46 +0200

    Single requirements.txt

--- examples/test.sh
+++ examples/test.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 cd  `dirname $0`
-python3 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
 ansible-galaxy collection install -p ./ ../freedge-acs-*.tar.gz
 fake-switches &
 ID=$!
 ansible-playbook acs.yml --check
 ret=$?
 kill $ID
-deactivate
 exit $ret
 
 
