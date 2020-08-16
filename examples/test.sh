8d2dbbefc25211b30bcbd076a5bb3be109df0f34
François Rigault <rigault.francois@gmail.com>
2020-08-16 11:15:18 +0200

    Add fake switch into known hosts

--- examples/test.sh
+++ examples/test.sh
@@ -3,6 +3,9 @@ cd  `dirname $0`
 ansible-galaxy collection install -p ./ ../freedge-acs-*.tar.gz
 fake-switches &
 ID=$!
+sleep 1
+mkdir ~/.ssh ; touch ~/.ssh/known_hosts
+ssh-keyscan -T 2 -p 2222 -t rsa 127.0.0.1 >> ~/.ssh/known_hosts
 ansible-playbook acs.yml --check
 ret=$?
 kill $ID
